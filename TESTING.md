1a50fb619043786bd959347647f366eec46f4944
kelton1024 <your.email@example.com>
2026-01-01 15:51:12 -0500

    Added code to generate sql file and upload ddl to postgers

--- TESTING.md
+++ TESTING.md
@@ -2,10 +2,15 @@
 
 Insert
 ```bash
-// curl -X POST localhost:8080/insert -H "Content-Type: application/json" -d '{"key": "MyKey", "value": "Test"}'
+curl -X POST localhost:8080/insert -H "Content-Type: application/json" -d '{"key": "MyKey", "value": "Test"}'
 ```
 
 Query
 ```bash
-// curl -X POST localhost:8080/query -H "Content-Type: application/json" -d '{"key": "test"}'
+curl -X POST localhost:8080/query -H "Content-Type: application/json" -d '{"key": "test"}'
+```
+
+Run Postgres Container
+```bash
+podman run -d --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword postgres:14.19
 ```
---
+++ ddl/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+const (
+	outputFileName  = "gen_range_tracker_relational_model.sql"
+	jsonDirectory   = "../json/"
+	ddlDirectory    = "./"
+	databaseName    = "range_tracker"
+	rootDatabaseURL = "postgres://postgres:mysecretpassword@localhost:5432"
+)
+
+func main() {
+	args := os.Args[1:]
+	action := args[0]
+
+	switch action {
+	case "generate":
+		outputFile, err := os.Create(outputFileName)
+		if err != nil {
+			log.Fatalf("failed to create output file with the following error %v", err)
+		}
+
+		ddlSlice, err := generateDDL()
+		if err != nil {
+			log.Fatalf("failed to create DDL with the following error %v", err)
+		}
+
+		for _, sql := range ddlSlice {
+			outputFile.Write([]byte(sql))
+		}
+
+	case "create":
+		ddlSlice, err := generateDDL()
+		if err != nil {
+			log.Fatalf("failed to create DDL with the following error %v", err)
+		}
+
+		err = createDatabase(ddlSlice)
+		if err != nil {
+			log.Fatalf("failed to create database with the following error %v", err)
+		}
+	default:
+		log.Fatalf("invalid option was provided")
+	}
+}
