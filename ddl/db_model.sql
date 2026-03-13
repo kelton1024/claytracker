b607c18f40c58972f81cdd49ea4bef2f68912e13
cjosh-vt <cjosh@vt.edu>
2026-03-12 21:00:09 -0400

    updated data model for new erd

---
+++ ddl/db_model.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS states CASCADE;
+CREATE TABLE states (
+	state_id smallint primary key generated always as identity,
+	name text
+);
+
+DROP TABLE IF EXISTS ranges CASCADE;
+CREATE TABLE ranges(
+	range_id smallint primary key generated always as identity,
+	address1 text,
+	address2 text,
+	city text,
+	name text,
+	zipcode numeric(5),
+	state_id smallint,
+	lat float,
+	lng float,
+	CONSTRAINT fk_state_range FOREIGN KEY (state_id) REFERENCES states(state_id)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	user_id smallint primary key generated always as identity,
+	username text,
+	first_name text,
+	last_name text,
+	email text,
+	address1 text,
+	address2 text,
+	zipcode numeric(5),
+	state_id smallint,
+	password_hash text,
+	password_salt text,
+	CONSTRAINT fk_state_user FOREIGN KEY (state_id) REFERENCES states(state_id)
+);
+
+DROP TABLE IF EXISTS outings CASCADE;
+CREATE TABLE outings(
+	outing_id smallint primary key generated always as identity,
+	range_id smallint,
+	starttime TIMESTAMPTZ,
+	owner smallint,
+	CONSTRAINT fk_range_outing FOREIGN KEY (range_id) REFERENCES ranges(range_id)
+);
+
+DROP TABLE IF EXISTS stations CASCADE;
+CREATE TABLE stations(
+	station_id smallint primary key generated always as identity,
+	range_id smallint,
+	lat float,
+	lng float,
+	CONSTRAINT fk_range_station FOREIGN KEY (range_id) REFERENCES ranges(range_id)
+);
+
+DROP TABLE IF EXISTS groupings;
+CREATE TABLE groupings(
+	groups_id smallint primary key generated always as identity,
+	outing_id smallint,
+	name text,
+	CONSTRAINT fk_outing_group FOREIGN KEY (outing_id) REFERENCES outings(outing_id)
+);
